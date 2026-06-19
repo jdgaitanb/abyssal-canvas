6b8f08ccde84fdaf7fc208970de8c6bd1a9ec20b
jdgaitanb <juan.david.gaitan.brieva@students.iaac.net>
2026-06-19 18:29:13 +0200

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -10,8 +10,6 @@ export const getRouter = () => {
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
-    // Adapta los enlaces internos automáticamente según el entorno (Lovable o GitHub Pages)
-    basepath: import.meta.env.BASE_URL || "/",
   });
 
   return router;
